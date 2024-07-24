6dd4d289f8cec7a6e7c6318b690d5c0591d69112
MeronMEndris <meronmohammed00@gmail.com>
2024-07-23 21:14:11 -0500

    initial commit

---
+++ src/main/java/com/anneye/hagere/HagereApplication.java
@@ -0,0 +1,13 @@
+package com.anneye.hagere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HagereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HagereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anneye/hagere/HagereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anneye.hagere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HagereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
